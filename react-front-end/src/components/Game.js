22896d0a6587ad88694882d39a9409964929296e
Melissa <mawilliamson10@gmail.com>
2022-03-21 17:14:44 +0000

    add components

--- react-front-end/src/components/Game.js
+++ react-front-end/src/components/Game.js
@@ -6,10 +6,11 @@ export default function Game() {
   useEffect(() => {
     console.log("canvasRef", canvasRef.current)
   }, [])
-
-
+  
   return (
+
     <body>
+      
       <div id="wrapper">
         <video id="camera" width="320" height="240" autoplay="true"></video>
         <canvas ref={canvasRef} id="overlay" width="320" height="240"></canvas>
