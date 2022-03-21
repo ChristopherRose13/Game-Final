b4f2df6b64d4371d052a21fb2b92c9dc2cd90f88
Christopher Rose <48345024+ChristopherRose13@users.noreply.github.com>
2022-03-21 12:50:21 -0700

    add loading bar and create play button functionality

--- react-front-end/src/game/intro.js
+++ react-front-end/src/game/intro.js
@@ -129,6 +129,52 @@ export default function phaserGame () {
   
   function preload ()
   {
+  var width = this.cameras.main.width;
+  var height = this.cameras.main.height;
+  var loadingText = this.make.text({
+    x: width / 2,
+    y: height / 2 - 50,
+    text: 'Loading...',
+    style: {
+        font: '20px monospace',
+        fill: '#ffffff'
+    }
+  });
+
+  var percentText = this.make.text({
+    x: width / 2,
+    y: height / 2 - 5,
+    text: '0%',
+    style: {
+        font: '18px monospace',
+        fill: '#ffffff'
+    }
+});
+  percentText.setOrigin(0.5, 0.5);
+  loadingText.setOrigin(0.5, 0.5);
+  this.load.on('progress', function (value) {
+    console.log(value);
+    progressBar.clear();
+    progressBar.fillStyle(0xffffff, 1);
+    progressBar.fillRect(250, 280, 300 * value, 30);
+    percentText.setText(parseInt(value * 100) + '%');
+  });
+              
+  this.load.on('fileprogress', function (file) {
+      console.log(file.src);
+      
+  });
+  this.load.on('complete', function () {
+      console.log('complete');
+      progressBar.destroy();
+      progressBox.destroy();
+      loadingText.destroy();
+      percentText.destroy();
+  });
+  var progressBar = this.add.graphics();
+  var progressBox = this.add.graphics();
+  progressBox.fillStyle(0x222222, 0.8);
+  progressBox.fillRect(240, 270, 320, 50);
   
   this.load.image('ground', 'assets/platform.png');
   this.load.image('sky', 'assets/sky.png');
