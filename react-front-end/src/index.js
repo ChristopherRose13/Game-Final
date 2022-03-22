2b8593b949bf63747b58f4aadd7c286542c30786
willianchu <willianchu@hotmail.com>
2022-03-22 11:33:47 -0700

    set a Button Nav Bar into the project

--- react-front-end/src/index.js
+++ react-front-end/src/index.js
@@ -1,22 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-import Phaser from 'phaser';
+import './index.css';
+import NavProvider from "./providers/NavProvider";
+// comment out it to use the previous code
+import * as serviceWorker from './serviceWorker'; // What is this?
+// import Phaser from 'phaser';
 // import configFunction from "./game/intro"
-import tracking from "jstracking";
-import annyang from "annyang";
-import { isListening } from 'annyang';
-import phaserGame from "./game/intro";
-
+// import tracking from "jstracking";
+// import annyang from "annyang";
+// import { isListening } from 'annyang';
+// import phaserGame from "./game/intro";
 // const configuration = configFunction()
 // const game = new Phaser.Game(configuration);
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <NavProvider>
+  
+    <App />
+
+  </NavProvider>
+  ,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister(); // What is this?
---
+++ react-front-end/src/pages/howto.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavButtons from '../NavButtons';
+
+export default function HowTo() {
+
+  return (
+    <div className="howTo">
+      <NavButtons />
+      <h1>How To</h1>
+      <div> How To DIV is here </div>
+    </div>
+  );
+}
