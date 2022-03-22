fafe36e184bf7806404388c78a34b1d3ca381bd6
Melissa <mawilliamson10@gmail.com>
2022-03-22 18:53:51 +0000

    add file for navbar styling

--- react-front-end/src/NavButtons.js
+++ react-front-end/src/NavButtons.js
@@ -2,15 +2,17 @@ import React from 'react';
 import { useContext } from 'react';
 import { menuContext } from './providers/NavProvider';
 // import 'App.css';
+import './styles/menu.scss';
+
 
 export default function Selector() {
   const { selector, onPlay, onHowTo, onHighScores } = useContext(menuContext);
 
   return (
     <div>
-      <button onClick={onPlay}>PLAY</button>
-      <button onClick={onHowTo}>HowTo</button>
-      <button onClick={onHighScores}>HighScores</button>
+      <button className="buttons" onClick={onPlay}>PLAY</button>
+      <button className="buttons" onClick={onHowTo}>HowTo</button>
+      <button className="buttons" onClick={onHighScores}>HighScores</button>
       Context: <span className="Context"> {selector} </span>
     </div>
   );
