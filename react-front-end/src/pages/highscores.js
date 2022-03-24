3e8bd4736d73cb69ac9b06348f849e35275330d4
Melissa <mawilliamson10@gmail.com>
2022-03-24 03:21:16 +0000

    rank does not have to be top 10 to show your rank and score

--- react-front-end/src/pages/highscores.js
+++ react-front-end/src/pages/highscores.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import NavButtons from '../NavButtons';
-import { FaCamera, FaMicrophoneAlt, FaKeyboard } from 'react-icons/fa';
+import { FaCamera, FaMicrophoneAlt, FaKeyboard, FaMedal } from 'react-icons/fa';
 import getLeaderBoard from '../hooks/getLeaderBoard';
 import '../styles/highscores.scss';
 
 export default function Highscores() {
-  const data = getLeaderBoard();
+  const data = getLeaderBoard(); //all the data
   const localData = [...data];
-
+  console.log("INITIAL DATA", data)
+  const best = data.slice(0, 10)   //only the best 10
+  console.log("best 10::", best)
 
 
   const yourScore = function (data) {
@@ -23,30 +25,18 @@ export default function Highscores() {
 
   }
 
-
   const yourRank = function (data) {
-    let last = data[0];
-    let answer;
     let rank;
-    for (let i = 0; i < data.length; i++) {
-      if (last.id < data[i].id) {
-        last = data[i];
-      }
-      answer = last.score;
-      rank = data.findIndex(x => x.score === answer) + 1;
-    }
-
+    let answer = yourScore(data);
+    rank = data.findIndex(x => x.score === answer) + 1;
     return rank;
-  }
 
+  }
 
 
   const playerScore = yourScore(data);
   const rank = yourRank(data);
   console.log("playerScore!!", playerScore)
-
-
-
 
 
   if (!localData === []) {
@@ -61,21 +51,36 @@ export default function Highscores() {
 
     return (
       <body className="body">
+
+        <img src="../assets/pink-sky.png" alt="stars" className="background"></img>
+
         <div className="highScores">
           <NavButtons />
           <h1 className="leaderboard"> Leaderboard </h1>
           <table className="table">
             <thead>
-              <tr className="heading">
-                <th>Rank</th>
-                <th>Name</th>
-                <th>Mode</th>
-                <th>Score</th>
+              <tr>
+                <th className="heading">Rank</th>
+                <th className="heading">Name</th>
+                <th className="heading">Mode</th>
+                <th className="heading">Score</th>
               </tr>
             </thead>
             <tbody className="results">
-              {data.map((d, i) => <tr key={i}>
-                <td>{i + 1}</td>
+              {best.map((d, i) => <tr key={i}>
+                <td>
+                  {i === 0 &&
+                    <FaMedal className="gold" />
+                  }
+                  {i === 1 &&
+                    <FaMedal className="silver" />
+                  }
+                  {i === 2 &&
+                    <FaMedal className="bronze" />
+                  }
+                  {i + 1}
+
+                </td>
                 <td>{d.name}</td>
                 <td>
                   <span>
@@ -88,7 +93,7 @@ export default function Highscores() {
               </tr>)}
             </tbody>
           </table>
-          <h2 className="your-score">Your score:  {playerScore} </h2>
+          <h2 className="your-score">Your score: {playerScore} </h2>
           <h2 className="your-rank">You are ranked # {rank}!</h2>
         </div>
       </body>)
